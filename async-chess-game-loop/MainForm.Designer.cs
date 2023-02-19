d05beae75d88430c5e31fbe26cf00eeebb84b822
Tom Gregor <tgregor@ivsoftware.com>
2023-02-19 13:09:51 -0700

    Add project files.

---
+++ async-chess-game-loop/MainForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace async_chess_game_loop
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this._opponentClock = new async_chess_game_loop.ChessClock();
+            this._playerClock = new async_chess_game_loop.ChessClock();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel.ColumnCount = 8;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel.Location = new System.Drawing.Point(121, 23);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.RowCount = 8;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(500, 500);
+            this.tableLayoutPanel.TabIndex = 0;
+            // 
+            // _opponentClock
+            // 
+            this._opponentClock.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this._opponentClock.Location = new System.Drawing.Point(20, 23);
+            this._opponentClock.Name = "_opponentClock";
+            this._opponentClock.Size = new System.Drawing.Size(88, 38);
+            this._opponentClock.TabIndex = 1;
+            this._opponentClock.Text = "0:00:00";
+            this._opponentClock.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // _playerClock
+            // 
+            this._playerClock.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this._playerClock.Location = new System.Drawing.Point(20, 485);
+            this._playerClock.Name = "_playerClock";
+            this._playerClock.Size = new System.Drawing.Size(88, 38);
+            this._playerClock.TabIndex = 1;
+            this._playerClock.Text = "0:00:00";
+            this._playerClock.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox.BackColor = System.Drawing.Color.White;
+            this.richTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.richTextBox.Location = new System.Drawing.Point(639, 23);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.ReadOnly = true;
+            this.richTextBox.Size = new System.Drawing.Size(170, 497);
+            this.richTextBox.TabIndex = 2;
+            this.richTextBox.TabStop = false;
+            this.richTextBox.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 544);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this._playerClock);
+            this.Controls.Add(this._opponentClock);
+            this.Controls.Add(this.tableLayoutPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "MainForm";
+            this.Text = "Chess";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+        private ChessClock _opponentClock;
+        private ChessClock _playerClock;
+        private System.Windows.Forms.RichTextBox richTextBox;
+    }
+}
---
+++ async-chess-game-loop/MainForm.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace async_chess_game_loop
+{
+    enum PlayerColor
+    {
+        White,
+        Black,
+    }
+    enum StateOfPlay
+    {
+        PlayerChooseFrom,
+        PlayerChooseTo,
+        OpponentTurn,
+    }
+    public partial class MainForm : Form
+    {
+        public MainForm() => InitializeComponent();
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            // Fix rounding.
+            tableLayoutPanel.Size = new Size(507, 507);
+            for (int column = 0; column < 8; column++)
+                for (int row = 0; row < 8; row++)
+                    addSquare(row, column);
+            // Random draw
+            PlayerColor playerColor =  (PlayerColor)_rando.Next(2);
+            Text = $"Player is {playerColor}";
+            _ = playGameAsync(playerColor);
+        }
+
+        Random _rando = new Random();
+        SemaphoreSlim _semaphoreClick= new SemaphoreSlim(0, 1);
+
+        private async Task playGameAsync(PlayerColor playerColor)
+        {
+            StateOfPlay = 
+                playerColor.Equals(PlayerColor.White) ?
+                    StateOfPlay.PlayerChooseFrom :
+                    StateOfPlay.OpponentTurn;
+
+            while(!_checkmate)
+            {
+                if(StateOfPlay.Equals(StateOfPlay.OpponentTurn))
+                {
+                    await opponentMove();
+                }
+                else
+                {
+                    await _semaphoreClick.WaitAsync();
+                }
+            }
+        }
+        StateOfPlay _stateOfPlay = (StateOfPlay)(-1);
+        StateOfPlay StateOfPlay
+        {
+            get => _stateOfPlay;
+            set
+            {
+                if (!Equals(_stateOfPlay, value))
+                {
+                    _stateOfPlay = value;
+                    switch (StateOfPlay)
+                    {
+                        case StateOfPlay.PlayerChooseFrom:
+                            _opponentClock.Stop();
+                            _ = _playerClock.Start();
+                            break;
+                        case StateOfPlay.OpponentTurn:
+                            _playerClock.Stop();
+                            _ = _opponentClock.Start();
+                            break;
+                    }
+                }
+            }
+        }
+
+        bool _checkmate = false;
+
+        Square
+            _playerFrom,
+            _playerTo,
+            _opponentFrom,
+            _opponentTo;
+        private void onSquareClicked(object sender, EventArgs e)
+        {
+            if (sender is Square square)
+            {
+                switch (StateOfPlay)
+                {
+                    case StateOfPlay.OpponentTurn:
+                        // Disabled for opponent turn
+                        return;
+                    case StateOfPlay.PlayerChooseFrom:
+                        _playerFrom = square;
+                        Text = $"Player {_playerFrom.Notation} : _";
+                        StateOfPlay = StateOfPlay.PlayerChooseTo;
+                        break;
+                    case StateOfPlay.PlayerChooseTo:
+                        _playerTo = square;
+                        Text = $"Player {_playerFrom.Notation} : {_playerTo.Notation}";
+                        richTextBox.SelectionColor = Color.DarkGreen;
+                        richTextBox.AppendText($"{_playerFrom.Notation} : {_playerTo.Notation}{Environment.NewLine}");
+                        StateOfPlay = StateOfPlay.OpponentTurn;
+                        break;
+                }
+                _semaphoreClick.Release();
+            }
+        }
+
+        private async Task opponentMove()
+        {
+            Text = "Opponent thinking";
+            
+            for (int i = 0; i < _rando.Next(5, 10); i++)
+            {
+                Text += ".";
+                await Task.Delay(1000);
+            }
+            string opponentMove = "xx : xx";
+            Text = $"Opponent Moved {opponentMove}";
+            richTextBox.SelectionColor = Color.DarkBlue;
+            richTextBox.AppendText($"{opponentMove}{Environment.NewLine}");
+            StateOfPlay = StateOfPlay.PlayerChooseFrom;
+        }
+
+
+        private void addSquare(int column, int row)
+        {
+            var color = ((column + row) % 2) == 0 ? Color.White : Color.Black;
+            var square = new Square
+            {
+                BackColor = color,
+                Column = column,
+                Row = row,
+                Size = new Size(80, 80),
+                Margin = new Padding(0),
+                Padding = new Padding(10),
+                Anchor = (AnchorStyles)0xf,
+                SizeMode = PictureBoxSizeMode.StretchImage,
+            };
+            tableLayoutPanel.Controls.Add(square, column, row);
+            // Hook the mouse events here
+            square.Click += onSquareClicked;
+        }
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                const int WS_EX_COMPOSITED = 0x02000000;
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= WS_EX_COMPOSITED;
+                return cp;
+            }
+        }
+    }
+
+    class ChessClock : Label
+    {
+        Stopwatch _stopwatch= new Stopwatch();
+        internal async Task Start()
+        {
+            BackColor= Color.LightBlue;
+            _stopwatch.Start();
+            while(_stopwatch.IsRunning)
+            {
+                await Task.Delay(500);
+                Text = _stopwatch.Elapsed.ToString(@"h\:mm\:ss");
+            }
+        }
+
+        internal void Stop()
+        {
+            BackColor= SystemColors.Control;
+            _stopwatch.Stop();
+        }
+    }
+
+    class Square : PictureBox 
+    {
+        public int Column { get; internal set; }
+        public int Row { get; internal set; }
+        public string Notation => $"{(char)(Column + 'a')}{8 - Row}";
+        public override string ToString() =>
+            Piece == Piece ?
+                $"Empty {BackColor.Name} square [column:{Column} row:{Row}]" :
+                $"{Piece} piece [column:{Column} row:{Row}]";
+        public Piece Piece { get; set; }
+    }
+    enum Player { Black, White };
+    enum PieceType { Pawn, Rook, Knight, Bishop, Queen, King }
+    class Piece
+    {
+        public Player Player { get; internal set; }
+        public PieceType PieceType { get; internal set; }
+        public override string ToString() =>
+            $"{Player} {PieceType}";
+    }
+}
